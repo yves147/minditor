12c700b91b00f346f449eea1e871954bd02f5508
Yves <yves.j@outlook.de>
2021-03-04 21:01:13 +0100

    fixed require error (commit may include some useless files; will remove them asap)

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
-const { app, BrowserWindow } = require('electron')
+const {
+  app,
+  BrowserWindow,
+  ipcMain
+} = require("electron");
+const path = require("path");
+const fs = require("fs");
 
-function createWindow() {
-  const win = new BrowserWindow({
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+async function createWindow() {
+
+  // Create the browser window.
+  win = new BrowserWindow({
     width: 800,
     height: 600,
-    darkTheme: true,
     webPreferences: {
-      nodeIntegration: true,
-      enableRemoteModule: true
+      nodeIntegration: false, // is default value after Electron v5
+      contextIsolation: true, // protect against prototype pollution
+      enableRemoteModule: true, // turn off remote
+      preload: path.join(__dirname, "preload.js") // use a preload script
     }
-  })
+  });
 
-  win.loadFile('index.html')
+  // Load app
+  win.loadFile(path.join(__dirname, "index.html"));
+
+  // rest of code..
 }
 
-app.whenReady().then(createWindow)
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+ipcMain.on("toMain", (event, args) => {
+  fs.readFile("./.gitignore", (error, data) => {
+    // Do something with file contents
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+    // Send result back to renderer process
+    win.webContents.send("fromMain", data.toString());
+  });
+});
